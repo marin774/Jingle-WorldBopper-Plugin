60588b200c91405655383be5f4570df611515f87
marin774 <marinbanozic@gmail.com>
2025-12-31 13:44:00 +0100

    bump version to v2.1.0

--- src/main/java/me/marin/worldbopperplugin/util/UpdateUtil.java
+++ src/main/java/me/marin/worldbopperplugin/util/UpdateUtil.java
@@ -21,7 +21,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 
 import static me.marin.worldbopperplugin.WorldBopperPlugin.*;
 
